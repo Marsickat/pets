5894b518713fba3572e2ac6eb97f2dc40816f05b
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-18 16:00:26 +0300

    Дней до дня рождения v1.2
    Убрал вывод дней рождения, добавил вывод текущей даты

--- days_until_birthday.py
+++ days_until_birthday.py
@@ -3,4 +3,6 @@ import datetime as dt
 vlad_birthday = dt.date(1996, 10, 9)
 igor_birthday = dt.date(1998, 6, 28)
 
-print(vlad_birthday, igor_birthday, sep="\n")
+today = dt.date.today()
+
+print(today)
