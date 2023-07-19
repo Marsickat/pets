28deeb6b1a2bda789dee42f529a1a3178b42d79e
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-19 11:07:01 +0300

    Дней до дня рождения v1.4
    Начал реализацию функции для вычисления количества дней до дня рождения

--- days_until_birthday.py
+++ days_until_birthday.py
@@ -1,16 +1,23 @@
 import datetime as dt
 
+
+def get_days_to_birthday(date_birthday: dt.date) -> int:
+    """Получает дату дня рождения и возвращает количество оставшихся дней до этой даты.
+
+    :param date_birthday: Дата дня рождения
+    :type date_birthday: datetime.date
+    :return: Количество оставшихся дней до дня рождения
+    :rtype: int
+    """
+    today = dt.date.today()  # Получение сегодняшней даты
+    today_year = today.year  # Получение текущего года
+    date_birthday = date_birthday.replace(year=today_year)  # Заменяем текущее значение года в дне рождения
+    days_left = (date_birthday - today).days  # Вычисляем количество оставшихся дней
+    return days_left
+
+
 vlad_birthday = dt.date(1996, 10, 9)  # День рождения Влада
 igor_birthday = dt.date(1998, 6, 28)  # День рождения Игоря
 
-today = dt.date.today()  # Получение сегодняшней даты
-today_year = today.year  # Получение текущего года
 
-# Заменяем текущие значения года в днях рождения
-vlad_birthday = vlad_birthday.replace(year=today_year)
-igor_birthday = igor_birthday.replace(year=today_year)
-
-vlad_days_left = vlad_birthday - today
-igor_days_left = igor_birthday - today
-
-print(vlad_days_left.days, igor_days_left.days, sep="\n")
+print(get_days_to_birthday(vlad_birthday), get_days_to_birthday(igor_birthday), sep="\n")
