497ff025775fd4f0c98ab57a86d106fed3544ca3
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-20 10:36:54 +0300

    Записная книжка v2.0
    Перенес функцию вывода информации о контактах в класс

--- notebook.py
+++ notebook.py
@@ -14,23 +14,15 @@ class Contact:
         self.address = address
         self.birthday = birthday
 
+    def show_contact(self) -> None:
+        """
+        Выводит информацию о контактах.
+        """
+        print(f"{self.name} - адрес: {self.address}, телефон: {self.phone}, день рождения: {self.birthday}")
+
 
 mike = Contact("Михаил Булгаков", "2-03-27", "Россия, Москва, Большая Пироговская, дом 35б, кв. 6", "15.05.1891")
 vlad = Contact("Владимир Маяковский", "73-88", "Россия, Москва, Лубянский проезд, д. 3, кв. 6", "19.07.1893")
 
-
-def print_contact():
-    """
-    Выводит информацию о контактах.
-    """
-    print(f"{mike.name} - адрес: {mike.address}, телефон: {mike.phone}, день рождения: {mike.birthday}")
-    print(f"{vlad.name} - адрес: {vlad.address}, телефон: {vlad.phone}, день рождения: {vlad.birthday}")
-
-
-mike.address = "Россия, Москва, Нащокинский переулок, дом 3, кв. 44"
-mike.phone = "К-058-67"
-
-vlad.address = "Россия, Москва, Гендриков переулок, дом 15, кв. 5"
-vlad.phone = "2-35-71"
-
-print_contact()
+mike.show_contact()
+vlad.show_contact()
