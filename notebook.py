d108542f5f354677821db287b2f6eb50f6766812
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-20 10:06:18 +0300

    Записная книжка v1.1
    Добавил изменения в адрес и телефон тестовых контактов

--- notebook.py
+++ notebook.py
@@ -1,5 +1,14 @@
 class Contact:
     def __init__(self, name: str, phone: str, address: str, birthday: str):
+        """
+        Создает новый объект класса Contact.
+
+        Параметры:
+            name (str): Имя контакта.
+            phone (str): Номер телефона контакта.
+            address (str): Адрес контакта.
+            birthday (str): Дата рождения контакта.
+        """
         self.name = name
         self.phone = phone
         self.address = address
@@ -11,8 +20,17 @@ vlad = Contact("Владимир Маяковский", "73-88", "Россия, 
 
 
 def print_contact():
+    """
+    Выводит информацию о контактах.
+    """
     print(f"{mike.name} - адрес: {mike.address}, телефон: {mike.phone}, день рождения: {mike.birthday}")
     print(f"{vlad.name} - адрес: {vlad.address}, телефон: {vlad.phone}, день рождения: {vlad.birthday}")
 
+
+mike.address = "Россия, Москва, Нащокинский переулок, дом 3, кв. 44"
+mike.phone = "К-058-67"
+
+vlad.address = "Россия, Москва, Гендриков переулок, дом 15, кв. 5"
+vlad.phone = "2-35-71"
 
 print_contact()
